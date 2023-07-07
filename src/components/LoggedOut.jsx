c464f23048bca7665ad80d3510331a00681bf83c
kacper <93041597+haster09@users.noreply.github.com>
2023-07-07 14:59:16 +0200

    finishes header

--- src/components/LoggedOut.jsx
+++ src/components/LoggedOut.jsx
@@ -1,6 +1,11 @@
+import icon from '../assets/loggedOutIcon.svg';
+
 const LoggedOut = () => {
   return (
-    <div className="loggedOut"></div>
+    <div className="loggedOut">
+      <div className='loggedOutIconDiv'><img className='loggedOutIcon' alt='icon' src={icon}/></div>
+      <div className="loggedOutText">Sign in</div>
+    </div>
   )
 }
 
